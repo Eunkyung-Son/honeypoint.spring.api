aef2c631aee80404bd919fe5f58e4ff2cb9983e3
Eunkyung-Son <sek9510@naver.com>
2021-10-30 17:35:35 +0900

    멤버 id로 review 가져오기 쿼리 수정

--- src/main/java/com/ek/honeypoint/models/GnrlMember.java
+++ src/main/java/com/ek/honeypoint/models/GnrlMember.java
@@ -1,5 +1,7 @@
 package com.ek.honeypoint.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +10,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GnrlMember {
+	@JsonProperty("mNo")
 	private int mNo;
+	@JsonProperty("mNickname")
 	private String mNickname;
+	@JsonProperty("mBirthday")
 	private int mBirthday;
+	@JsonProperty("mPhone")
 	private int mPhone;
+	@JsonProperty("mAddress")
 	private String mAddress;
+	@JsonProperty("mGrad")
 	private int mGrad;
+	@JsonProperty("mPoint")
 	private int mPoint;
 }
